d3e325d82ba0c12c69770d5231b7b36a076ba7c3
Soneech <sonya.patrina@yandex.ru>
2024-03-05 22:07:43 +0300

    Fixes for API doc

--- bot/src/main/java/edu/java/bot/controller/ApiController.java
+++ bot/src/main/java/edu/java/bot/controller/ApiController.java
@@ -11,13 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 // for swagger doc
 public interface ApiController {
-    String MEDIA_TYPE = "application/json";
 
     @ApiResponse(responseCode = "200", description = "Обновление обработано",
-                 content =
-                 @Content(mediaType = MEDIA_TYPE, schema = @Schema(implementation = SuccessMessageResponse.class)))
+                 content = @Content(schema = @Schema(implementation = SuccessMessageResponse.class)))
     @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса",
-                 content =
-                 @Content(mediaType = MEDIA_TYPE, schema = @Schema(implementation = ApiErrorResponse.class)))
+                 content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     SuccessMessageResponse handleUpdate(@RequestBody @Valid LinkUpdateRequest request);
 }
